eac2b1532ea1cc365c7f5046394fc36b8b13bed5
Oscar Barreto <ojbarreto9@hotmail.com>
2024-01-13 21:49:16 -0500

    cambio bootstrap

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -6,11 +6,11 @@ const Item = ({item}) => {
   return (
     <Link to={'/item/' + item.id} className='text-decoration-none'>
     
-    <div className='container'>
+    <div className='container mt-15'>
 
-        <div className='card border border-1'>
+        <div className='card border border-1 p-3 mb-5' style={{ height: '20rem' }}>
             <img src={item.img} className='card-img-top img-fluid' alt={item.title} />
-            <div className='card-body text-center'>
+            <div className='card-body text-center mt-3'>
                 <h4 children='card-text'>{item.categoryId} {item.title}</h4>
                 <p className='card-text'>$ {item.price} USD </p>
 
